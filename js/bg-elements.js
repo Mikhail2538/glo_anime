c221dba6e585aa6bc68b9591058cbd7c6f9fc0fa
Mikhail Gr <g_ms@bk.ru>
2026-03-08 22:28:39 +0300

    Второй урок

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,7 +1,9 @@
-const elements = document.querySelectorAll(".set-bg");
-
-for (let i = 0; i < elements.length; i++) {
-  const src = elements[i].dataset.setbg;
-  elements[i].style.backgroundImage = `url(${src})`;
-  console.dir(elements[i]);
-}
+const bgElements = () => {
+  const elements = document.querySelectorAll(".set-bg");
+  for (let i = 0; i < elements.length; i++) {
+    const src = elements[i].dataset.setbg;
+    elements[i].style.backgroundImage = `url(${src})`;
+    console.dir(elements[i]);
+  }
+};
+bgElements();
